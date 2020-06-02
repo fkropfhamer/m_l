26938af9c1421c6b8479e4e904161b274267b833
fabian <fabian.kropfhamer@gmail.com>
2020-06-02 19:48:00 +0200

    implemented update and predict

--- src/perceptron.py
+++ src/perceptron.py
@@ -5,9 +5,32 @@ class Perceptron:
 
     def fit(self, feature_set, labels):
         self.is_trained = True
+        self.weights = [1 for i in range(len(feature_set[0]))]
+
         return self
 
+    def update(self, features, label):
+        predicition = self.predict(features)
+
+        if predicition and not label:
+            error = 1
+        elif not predicition and label:
+            error = -1
+        else:
+            return
+
+        self.weights = [w - x * error for w, x in zip(self.weights, features)]
+        
+
+        
+
+
     def predict(self, features):
+        score = sum([x * y for x, y in zip(features, self.weights)])
+
+        if score > self.bias:
+            return 1
+        
         return 0
 
 
--- src/test_perceptron.py
+++ src/test_perceptron.py
@@ -16,9 +16,40 @@ class PeceptronTest(unittest.TestCase):
 
         p = Perceptron()
 
-        p.fit([], [])
+        p.fit([[1, 2], [3, 4]], [0, 1])
 
         self.assertEqual(p.is_trained, True)
+        self.assertEqual(len(p.weights), 2)
+
+    def test_predict(self):
+        p = Perceptron()
+        p.weights = [1,2]
+
+        self.assertEqual(p.predict([1,2]), 1)
+
+        p.weights = [-1,-2]
+
+        self.assertEqual(p.predict([1,2]), 0)
+ 
+    def test_update(self):
+        p = Perceptron()
+
+        p.weights = [1,2]
+
+        p.update([2,2], 1)
+
+        self.assertEqual(p.weights, [1,2])
+
+        p.update([2,2], 0)
+
+        self.assertEqual(p.weights, [-1, 0])
+
+        p.weights = [-1,-2]
+
+        p.update([2,2], 1)
+
+        self.assertEqual(p.weights, [1, 0])
+
 
 
 if __name__ == "__main__":
