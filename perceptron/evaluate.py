986ef55625bce69906d6ae2ba5923f206a85538e
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-08-20 13:08:23 +0200

    added f1_score and recall

--- perceptron/evaluate.py
+++ perceptron/evaluate.py
@@ -9,3 +9,25 @@ def accuracy(feature_set, labels, predict_fn):
     
     return float(r) / len(feature_set)
     
+def precision(feature_set, labels, predict_fn):
+    return accuracy(feature_set, labels, predict_fn)
+
+def recall(feature_set, labels, predict_fn, correct_label=1):
+    r = 0
+
+    for features, label in zip(feature_set, labels):
+        prediction = predict_fn(features)
+
+        if prediction == label: 
+            r += 1 
+
+    number_of_correct_labels = len(filter(lambda x: x == correct_label, labels))
+
+    return float(r) / number_of_correct_labels
+
+def f1_score(feature_set, labels, predict_fn, correct_label=1):
+    p = precision(feature_set, labels, predict_fn)
+    r = recall(feature_set, labels, predict_fn, correct_label=correct_label)
+
+    return 2 * (p * r) / (p + r)
+    
