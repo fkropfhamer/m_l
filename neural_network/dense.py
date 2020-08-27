0b773f01a8adb7107094079bbbd7eb32f2a26c93
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-08-27 16:32:01 +0200

    cleanup

--- neural_network/dense.py
+++ neural_network/dense.py
@@ -1,12 +1,12 @@
 import numpy as np
 from neural_network.activation import relu
 
-class DenseLayer():
+
+class DenseLayer:
     def __init__(self, units, activation='relu'):
         self.weights = np.random.normal(size=units)
-		
-	self.activation = relu
+
+        self.activation = relu
 
     def feed_forward(self, inputs):
-	return self.activation(np.dot(self.weights, inputs) 
-
+        return self.activation(np.dot(self.weights, inputs))
